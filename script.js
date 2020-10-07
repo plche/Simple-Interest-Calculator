17ec4e1f671f659ba9347b01a5265decc1b79706
Percy Che <che.percy.l@gmail.com>
2020-10-07 06:35:29 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
 }
-        
+function fyear() {
+    return document.getElementById("years").value + 2020;
+}
+function display() {
+    var 
+}
