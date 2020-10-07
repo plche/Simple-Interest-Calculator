f14c5d91fc4f05869af0d1b450e7b3ff16ddde9a
Percy Che <che.percy.l@gmail.com>
2020-10-07 14:12:34 -0500

    Final code!

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function compute() {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-}
-function fyear() {
-    return document.getElementById("years").value + 2020;
+    var principal = document.getElementById("principal");
+    var rate = document.getElementById("rate");
+    var years = document.getElementById("years");
+    var interest = principal.value * years.value * rate.value / 100;
+    var fyear = 2020 + years.value * 1;
+    if (principal.value <= 0){
+    	alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }
+    return document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal.value + "\<\/mark\>,\<br\>" + "at an interest rate of \<mark\>" + rate.value + "%\<\/mark\>.\<br\>" + "You will receive an amount of \<mark\>" + interest + "\<\/mark\>,\<br\>" + "in the year \<mark\>" + fyear + "\<\/mark\>\<br\>\<br\>";
 }
 function display() {
-    var 
+    var rselected = document.getElementById("rate").value;
+    return document.getElementById("srate").innerHTML = rselected;
 }
